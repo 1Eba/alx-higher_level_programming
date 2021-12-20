d99c8cb66536e166c554cd67bed6869cfd2d91d8
1Eba <barkotka@gmail.com>
2021-12-20 22:25:29 +0000

    Inheritance 7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
-"""Integer validator
-Author: Barok
+""" Creates an empty class called BaseGeometry
 """
 
 
 class BaseGeometry:
-    """An empty class"""
+    def __init__(self):
+        pass
 
     def area(self):
-        """Raises an exception because...
-        area is not implemented
+        """raise an exception
         """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates the inputs: name and values
-        Args:
-            name(str) - input name as string
-            value(int): pararams validator
+        """validates value
         """
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-        return value
