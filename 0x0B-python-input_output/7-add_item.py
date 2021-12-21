4b6d698690fb6d04a8d35b580dd29880bf27c50a
1Eba <barkotka@gmail.com>
2021-12-21 23:08:20 +0000

    read file 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
+"""Load, add, save
+A script that adds all arguments to a Python list,
+and then save them to a file
+"""
+import sys
 
+if __name__ == "__main__":
+    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('8-load_from_json_file').load_from_json_file
 
-def save_to_json_file(my_obj, filename):
-    """writes an Object to a text file, using a JSON representation"""
-    import json
-
-    with open(filename, "w") as myFile:
-        myFile.write(json.dumps(my_obj))
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
